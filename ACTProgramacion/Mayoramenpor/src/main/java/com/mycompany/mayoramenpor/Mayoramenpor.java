473dbb75b5002cf055fff291c3269513a1daea1b
Luisluna1624 <luisalbertogrijalva24@gmail.com>
2025-01-27 12:18:53 -0700

    hola

---
+++ ACTProgramacion/Mayoramenpor/src/main/java/com/mycompany/mayoramenpor/Mayoramenpor.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mayoramenpor;
+
+/**
+ *
+ * @author Luis Alberto
+ */
+public class Mayoramenpor {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ Actividad1/src/main/java/com/mycompany/actividad1/Actividad1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.actividad1;
+
+/**
+ *
+ * @author Luis Alberto
+ */
+public class Actividad1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
